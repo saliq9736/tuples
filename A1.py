3e06b6f7353d26965a7411961ef47259f6f5813e
saliq9736 <asma.asad9736@gmail.com>
2026-06-10 10:02:58 -0700

    initial commit

---
+++ A1.py
@@ -0,0 +1,13 @@
+
+tuplex=("tuple",2,4,1,6)
+print(tuplex)
+tuplex=(1,5,3,7,3,8)
+print(tuplex)
+tuplex = tuplex +(9,)
+tuple2=(50,40,20,50,30)
+print(tuple2.count(50))
+tuplex=("saliq","daniyal", "haseeb","rehan","sheeraz")
+_slice= tuplex[1:3]
+print(_slice)
+_slice= tuplex[0:4:2]
+print(_slice)
